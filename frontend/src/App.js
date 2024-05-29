e35cbb5d806eb944a37df44c47e73cf1cec3de3d
Damaride Putignano <damarideputignano@gmail.com>
2024-05-29 15:06:54 +0200

    t

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Testo modificato 2
+          Testo modificato 33
         </a>
       </header>
     </div>
